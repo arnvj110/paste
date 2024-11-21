a2696843d362daee7703972fd1f39d0c2f14741a
Arnav Jain <arnavjain92105@gmail.com>
2024-11-21 16:34:34 +0530

    page8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./**/*.{js,jsx,ts,tsx}",
+    "./index.html",
   ],
   theme: {
     extend: {},
